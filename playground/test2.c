5a899bf3d2421f880f6bec593eb8683a17995eaf
Otto Andelin <oandelin@c3r1p2.hive.fi>
2023-07-21 13:16:01 +0300

    testing

--- playground/test2.c
+++ playground/test2.c
@@ -24,19 +24,15 @@ void *thread1_routine(void *data)
 	printf("%sThread [%ld]: wants lock l%s\n", YELLOW, tid, NC);
 	pthread_mutex_lock(&locks->lock1);
 	printf("%sThread [%ld]: owns lock 1%s\n", BYELLOW, tid, NC);
-	usleep(6000);
 	printf("%sThread [%ld]: wants lock 2%s\n", YELLOW, tid, NC);
 	pthread_mutex_lock(&locks->lock2);
 	printf("%sThread [%ld]: owns lock 2%s\n", BYELLOW, tid, NC);
-	
 	locks->count += 1;
-	
 	printf("%sThread [%ld]: unlocking lock 2%s\n", BYELLOW, tid, NC);
 	pthread_mutex_unlock(&locks->lock2);
 	printf("%sThread [%ld]: unlocking lock 1%s\n", BYELLOW, tid, NC);
 	pthread_mutex_unlock(&locks->lock1);
 	printf("%sThread [%ld]: finished%s\n", YELLOW, tid, NC);
-	usleep(3000);
 	return(NULL);
 }
 
@@ -61,7 +57,6 @@ void *thread2_routine(void *data)
 	printf("%sThread [%ld]: unlocking lock 1%s\n", BYELLOW, tid, NC);
 	pthread_mutex_unlock(&locks->lock1);
 	printf("%sThread [%ld]: finished%s\n", YELLOW, tid, NC);
-	usleep(800);
 	return(NULL);
 }
 
@@ -69,6 +64,9 @@ int main(void)
 {
 	pthread_t	tid1;
 	pthread_t	tid2;
+	pthread_t	tid3;
+	pthread_t	tid4;
+	pthread_t	tid5;
 	t_locks		locks;
 
 	locks.count = 0;
@@ -76,12 +74,26 @@ int main(void)
 	pthread_mutex_init(&locks.lock2, NULL);
 	pthread_create(&tid1, NULL, thread1_routine, &locks);
 	printf("Main: Created first thread [%ld]\n", tid1);
-	pthread_create(&tid2, NULL, thread2_routine, &locks);
+	pthread_create(&tid2, NULL, thread1_routine, &locks);
 	printf("Main: Created second thread [%ld]\n", tid2);
+	pthread_create(&tid3, NULL, thread1_routine, &locks);
+	printf("Main: Created third thread [%ld]\n", tid3);
+	pthread_create(&tid4, NULL, thread1_routine, &locks);
+	printf("Main: Created fourth thread [%ld]\n", tid4);
+	pthread_create(&tid5, NULL, thread1_routine, &locks);
+	printf("Main: Created fifth thread [%ld]\n", tid5);
+	
 	pthread_join(tid1, NULL);
 	printf("Main: Joined first thread[%ld]\n", tid1);
 	pthread_join(tid2, NULL);
-	printf("Main: Joined first thread[%ld]\n", tid2);
+	printf("Main: Joined second thread[%ld]\n", tid2);
+	pthread_join(tid3, NULL);
+	printf("Main: Joined third thread[%ld]\n", tid3);
+	pthread_join(tid4, NULL);
+	printf("Main: Joined fourth thread[%ld]\n", tid4);
+	pthread_join(tid5, NULL);
+	printf("Main: Joined fifth thread[%ld]\n", tid5);
+	
 	if(locks.count == 2)
 		printf("%sMain: OK. Total count is %d %s\n", GREEN, locks.count, NC);
 	else
